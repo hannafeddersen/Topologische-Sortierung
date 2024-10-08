feb9462d444e6f9de8037b34c6c0d634885e82c3
Your Name <hanna.feddersen@mni.thm.de>
2024-10-08 19:45:50 +0200

    add SortConversations.java class

---
+++ src/SortConversations.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortConversations {
+
+    public List<Integer> sort(int numConversation, List<int[]> conditions){
+        List<Integer> order = new ArrayList<>(); //Liste mit der ordnung
+        //Ein Graph hat nachbarn. Um diese zu tracken wird eine adjacent Liste verwendet
+
+        return order;
+    }
+}
